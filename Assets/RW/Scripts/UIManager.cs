9dc58ec07ae0543350288eea13169b0a2718248b
Jammerjab1 <89883568+Jammerjab1@users.noreply.github.com>
2021-11-30 20:01:15 -0500

    Gear rotation now works

--- Assets/RW/Scripts/UIManager.cs
+++ Assets/RW/Scripts/UIManager.cs
@@ -9,11 +9,13 @@ public class UIManager : MonoBehaviour
     public Animator settingsButton;
     public Animator dialog;
     public Animator contentPanel;
+    public Animator gearImage;
 
     public void ToggleMenu()
     {
         bool isHidden = contentPanel.GetBool("isHidden");
         contentPanel.SetBool("isHidden", !isHidden);
+        gearImage.SetBool("isHidden", !isHidden);
     }
 
     public void OpenSettings()
